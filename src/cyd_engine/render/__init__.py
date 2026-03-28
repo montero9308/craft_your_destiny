93c0f86367ee2f178373f490efda4ce2099da54d
montero9308 <58610816+montero9308@users.noreply.github.com>
2026-03-28 08:58:34 -0400

    Actualizar __init__.py

--- src/cyd_engine/render/__init__.py
+++ src/cyd_engine/render/__init__.py
@@ -1,1 +1,9 @@
-"""module"""
+# src/cyd_engine/render/__init__.py
+"""Módulo de renderizado."""
+
+from .camera import Camera
+from .terrain_renderer import TerrainRenderer
+from .grid_renderer import GridRenderer
+from .window import GameWindow
+
+__all__ = ["Camera", "TerrainRenderer", "GridRenderer", "GameWindow"]
